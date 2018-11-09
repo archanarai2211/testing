c1dfd76a18f80561a4edcd8cef5d81e6c381a18c
Archana Rai <archana.rai@sap.com>
2018-11-09 15:44:22 +0530

    Adding JS file from GitHub API

--- functions/ajname1/index.js
+++ functions/ajname1/index.js
@@ -1,5 +1,6 @@
-module.exports = {
-	handler: function (event, context) {
-		return "Test successful";
-	}
-};
+function add(){
+var a,b,c;
+a=Number(document.getElementById("first").value);
+b=Number(document.getElementById("second").value);
+c= a + b;
+}
